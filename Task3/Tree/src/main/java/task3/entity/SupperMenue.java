86eca9a5c92e725c23140ad19711bec88e7ed57c
OlgaSmolyakova <ollaniel@gmail.com>
2021-02-04 10:55:02 +0300

    Update SupperMenue.java

--- Task3/Tree/src/main/java/task3/entity/SupperMenue.java
+++ Task3/Tree/src/main/java/task3/entity/SupperMenue.java
@@ -1,6 +1,6 @@
 package task3.entity;
 
-public class SupperMenue extends Menue {
+public class SupperMenue extends Menue {//что это?
     public SupperMenue(int i, int i1, int i2, int i3, String s) {
         super();
     }
